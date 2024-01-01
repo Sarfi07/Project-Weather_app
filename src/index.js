edcee05654c06f20946f4a67e6bfdf8bc5b160b8
Sarfaroj Ansari <sarfarosha66@gmail.com>
2024-01-01 16:59:30 +0530

    fix lint

--- src/index.js
+++ src/index.js
@@ -3,40 +3,15 @@ import './styles/styles.css';
 const API_KEY = '53f8fe5738774873a1782717233012';
 const API_URL = 'https://api.weatherapi.com/v1/current.json';
 
-// todo
-  // loader
-  // styling
+async function fetchData(city = 'London') {
+  const response = await fetch(`${API_URL}?key=${API_KEY}&q=${city}`);
 
-function handleSearch() {
-  const input = document.getElementById('cityInput');
-  const btn = document.getElementById('cityInputBtn');
+  const data = await response.json();
 
-  btn.addEventListener('click', () => {
-    displayLoader();
-    
-    fetchData(input.value)
-    .then(data => {
-      setTimeout(() => {
-        removeLoader();
-        displayData(data);       
-      }, 1000);
-    })
-    .catch(err => alert(`City not found: ${input.value}`))
-  })
+  return data;
 }
-
-
-async function fetchData(city='London') {
-    const response = await fetch(`${API_URL}?key=${API_KEY}&q=${city}`);
-
-    const data = await response.json();
-
-    return data;
-}
-
 
 function displayData(data) {
-  const main = document.querySelector('main');
   const cityName = document.getElementById('cityName');
   const weatherPng = document.getElementById('weatherPng');
   const weatherValue = document.getElementById('weatherValue');
@@ -46,17 +21,8 @@ function displayData(data) {
   const text = document.getElementById('weatherText');
 
   const windSpeed = document.querySelector('#wind');
-  console.log(data)
 
-  let region = data.location.region;
-  if (region.length > 8) {
-    region = '';
-  }
-  else {
-    region = ', ' + region;
-  }
-  
-  cityName.textContent = `${data.location.name}${region}, ${data.location.country}`;
+  cityName.textContent = `${data.location.name}, ${data.location.country}`;
   weatherPng.src = data.current.condition.icon;
   weatherValue.textContent = `${data.current.temp_c}`;
   humidity.textContent = data.current.humidity;
@@ -64,7 +30,6 @@ function displayData(data) {
   feelslike.textContent = data.current.feelslike_c;
   windSpeed.textContent = data.current.wind_kph;
   text.textContent = data.current.condition.text;
-
 }
 
 function displayLoader() {
@@ -72,7 +37,7 @@ function displayLoader() {
 
   const loaderDiv = document.getElementById('loaderDiv');
 
-  loaderDiv.style.display = 'flex'
+  loaderDiv.style.display = 'flex';
   main.style.display = 'none';
 }
 
@@ -84,18 +49,37 @@ function removeLoader() {
   main.style.display = 'grid';
 }
 
+function handleSearch() {
+  const input = document.getElementById('cityInput');
+  const btn = document.getElementById('cityInputBtn');
 
-// default 
+  btn.addEventListener('click', () => {
+    displayLoader();
+
+    fetchData(input.value)
+      .then((data) => {
+        setTimeout(() => {
+          try {
+            removeLoader();
+            displayData(data);
+          } catch (err) {
+            alert('No location found');
+          }
+        }, 1000);
+      })
+      .catch((err) => alert(err.message));
+  });
+}
+
+// default
 displayLoader();
 
-
 fetchData()
-.then(data => {
-  setTimeout(() => {
-    removeLoader();
-    displayData(data);
-  }, 1000);
-})
+  .then((data) => {
+    setTimeout(() => {
+      removeLoader();
+      displayData(data);
+    }, 1000);
+  }).catch((err) => alert(err.message));
 
-
-handleSearch()
+handleSearch();
